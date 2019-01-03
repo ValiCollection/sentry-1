914ff816e6030e2aa22334bc06d21969ad95bd5f
Dan Fuller <dfuller@sentry.io>
2019-01-03 15:50:31 -0800

    feat(api): Implement organization level saved searches endpoint
    APP-942 #In-Code-Review Implement org level endpoints

---
+++ tests/sentry/api/endpoints/test_organization_searches.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+
+from django.utils import timezone
+from exam import fixture
+
+from sentry.api.serializers import serialize
+from sentry.models import SavedSearch
+from sentry.models.savedsearch import DEFAULT_SAVED_SEARCHES
+from sentry.testutils import APITestCase
+
+
+class OrganizationSearchesListTest(APITestCase):
+    endpoint = 'sentry-api-0-organization-searches'
+
+    @fixture
+    def user(self):
+        return self.create_user('test@test.com')
+
+    def test_simple(self):
+        self.login_as(user=self.user)
+        team = self.create_team(members=[self.user])
+        project1 = self.create_project(teams=[team], name='foo')
+        project2 = self.create_project(teams=[team], name='bar')
+
+        SavedSearch.objects.create(
+            project=project1,
+            name='bar',
+            query=DEFAULT_SAVED_SEARCHES[0]['query'],
+        )
+        included = [
+            SavedSearch.objects.create(
+                name='Global Query',
+                query=DEFAULT_SAVED_SEARCHES[0]['query'],
+                is_global=True,
+            ),
+            SavedSearch.objects.create(
+                project=project1,
+                name='foo',
+                query='some test',
+                date_added=timezone.now().replace(microsecond=0)
+            ),
+            SavedSearch.objects.create(
+                project=project1,
+                name='wat',
+                query='is:unassigned is:unresolved',
+                date_added=timezone.now().replace(microsecond=0)
+            ),
+            SavedSearch.objects.create(
+                project=project2,
+                name='foo',
+                query='some test',
+                date_added=timezone.now().replace(microsecond=0)
+            ),
+        ]
+
+        included.sort(key=lambda search: search.name)
+        response = self.get_valid_response(self.organization.slug)
+
+        assert response.data == serialize(included)
